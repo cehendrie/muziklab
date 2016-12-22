61f369f9b0df4669a4eb797391370d5e2280a621
Charles Hendrie <cehendrie@icloud.com>
2016-12-21 21:29:57 -0600

    Move todos from core to readme

--- README.rst
+++ README.rst
@@ -2,3 +2,11 @@ muziklib
 ========
 
 A simple python module for sorting and combining lists of music.
+
+TODO
+* add a test flag for validating music entries
+* each entry must have a band name, album name, year, and format
+* format should be validated against a known list of formats
+* validation for one file at a time
+* possibly add a config to define the entry's format
+
--- muziklib/core.py
+++ muziklib/core.py
@@ -1,15 +1,7 @@
 """
 core
-- a process script to read a file of album information and sort by band and release year
 
-TODO
-1. add command line parsing
-2. move inline classes to individual files
-2. allow music catalog file to be define on the command line
-3. allow multiple music catalog files be entered via config file
-4. determine various artists albums and sort various artist within file
-5. add format type to sort (possibly hierarchy of types)
-6. dynamically determine album input tokens
+A process script to read a file of album information and sort by band and release year
 """
 
 import os
